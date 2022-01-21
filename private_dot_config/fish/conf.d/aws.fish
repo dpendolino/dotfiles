4661bbe97a55e2685c577328c0845cb12f71f51f
Daniel Pendolino <dpendolino@springhealth.com>
2022-01-21 12:58:33 -0500

    make aws.fish quieter:

--- private_dot_config/fish/conf.d/aws.fish
+++ private_dot_config/fish/conf.d/aws.fish
@@ -2,6 +2,6 @@
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
 function ecr-login
-  hash docker && aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 205034600246.dkr.ecr.us-east-1.amazonaws.com
-  hash podman && aws ecr get-login-password --region us-east-1 | podman login --username AWS --password-stdin 205034600246.dkr.ecr.us-east-1.amazonaws.com
+  hash docker &>/dev/null && aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 205034600246.dkr.ecr.us-east-1.amazonaws.com
+  hash podman &>/dev/null && aws ecr get-login-password --region us-east-1 | podman login --username AWS --password-stdin 205034600246.dkr.ecr.us-east-1.amazonaws.com
 end
