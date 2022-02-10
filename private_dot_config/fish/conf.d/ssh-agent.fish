c9513579aec8a8991a8a0f5439194bf5227e40e5
Daniel Pendolino <dpendolino@springhealth.com>
2022-02-10 15:20:45 -0500

    send pgrep output to devnull

--- private_dot_config/fish/conf.d/ssh-agent.fish
+++ private_dot_config/fish/conf.d/ssh-agent.fish
@@ -1,3 +1,3 @@
 # set -x SSH_AUTH_SOCK /Users/danielpendolino/.ssh/agent
-pgrep ssh-agent || bass (ssh-agent) &> /dev/null
+pgrep ssh-agent &>/dev/null || bass (ssh-agent) &> /dev/null
 grep -slR "PRIVATE" ~/.ssh | xargs ssh-add &> /dev/null
