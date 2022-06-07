59b47845ed0bb5dfc216e1368bef69322aba042d
Daniel Pendolino <dpendolino@gmail.com>
2022-06-07 14:24:17 -0400

    add vim-go, earthly, and nvim-treesitter-context

--- private_dot_config/nvchad/lua/custom/plugins/init.lua
+++ private_dot_config/nvchad/lua/custom/plugins/init.lua
@@ -14,80 +14,80 @@ return {
   ["ellisonleao/glow.nvim"] = {
     config = function()
       cmd = "Glow"
-    end
+    end,
   },
-  [ "b3nj5m1n/kommentary" ] = {}, -- Simple plugins can be specified as strings
-  [ "ekickx/clipboard-image.nvim" ] = {}, -- paste images into neovim
-  [ "rhysd/git-messenger.vim" ] = {},
-  [ "svermeulen/vimpeccable" ] = {}, -- convenient lua functions
-  [ "JoosepAlviste/nvim-ts-context-commentstring" ] = {},
-  [ "ekalinin/Dockerfile.vim" ] = {}, -- Vim syntax file for Docker's Dockerfile and snippets for snipMate.
-  [ "junegunn/vim-peekaboo" ] = {},
-  [ "nacro90/numb.nvim" ] = {}, -- peek go to line
-  [ "williamboman/nvim-lsp-installer" ] = {}, -- Helper installer
-  [ "dstein64/vim-startuptime" ] = {}, -- startup profiler
-  [ "tpope/vim-surround" ] = {}, -- surround ops
-  [ "editorconfig/editorconfig-vim" ] = {},
-  [ "tpope/vim-repeat" ] = {}, -- let '.' work with other commands
-  [ "windwp/nvim-ts-autotag" ] = { -- autoclose and autorename html tags
+  ["b3nj5m1n/kommentary"] = {}, -- Simple plugins can be specified as strings
+  ["ekickx/clipboard-image.nvim"] = {}, -- paste images into neovim
+  ["rhysd/git-messenger.vim"] = {},
+  ["svermeulen/vimpeccable"] = {}, -- convenient lua functions
+  ["JoosepAlviste/nvim-ts-context-commentstring"] = {},
+  ["ekalinin/Dockerfile.vim"] = {}, -- Vim syntax file for Docker's Dockerfile and snippets for snipMate.
+  ["junegunn/vim-peekaboo"] = {},
+  ["nacro90/numb.nvim"] = {}, -- peek go to line
+  ["williamboman/nvim-lsp-installer"] = {}, -- Helper installer
+  ["dstein64/vim-startuptime"] = {}, -- startup profiler
+  ["tpope/vim-surround"] = {}, -- surround ops
+  ["editorconfig/editorconfig-vim"] = {},
+  ["tpope/vim-repeat"] = {}, -- let '.' work with other commands
+  ["windwp/nvim-ts-autotag"] = { -- autoclose and autorename html tags
     config = function()
       require("nvim-ts-autotag").setup()
     end,
   },
-  [ "dominikduda/vim_current_word" ] = {}, -- Highlight current word in buffer after delay}
-  [ "ntpeters/vim-better-whitespace" ] = { -- Deal with trailing whitespace
+  ["dominikduda/vim_current_word"] = {}, -- Highlight current word in buffer after delay}
+  ["ntpeters/vim-better-whitespace"] = { -- Deal with trailing whitespace
     config = function()
       vim.cmd("map <leader>s :StripWhitespace<CR>")
     end,
   },
-  [ "jose-elias-alvarez/null-ls.nvim" ] = {
+  ["jose-elias-alvarez/null-ls.nvim"] = {
     after = "nvim-lspconfig",
     config = function()
       require("custom.plugins.null-ls").setup()
     end,
   }, -- formatting
-  [ "folke/which-key.nvim" ] = {
+  ["folke/which-key.nvim"] = {
     config = function()
       require("custom.plugins.which-key").setup()
     end,
   },
-  [ "nathom/filetype.nvim" ] = {
+  ["nathom/filetype.nvim"] = {
     config = function()
       require("custom.plugins.filetype").setup()
     end,
   }, -- A fast and lua alternative to filetype.vim. It is ~175x faster than filetype.vim
-  [ "luukvbaal/stabilize.nvim" ] = {
+  ["luukvbaal/stabilize.nvim"] = {
     config = function()
       require("stabilize").setup()
     end,
   },
-  [ "lukas-reineke/cmp-rg" ] = {},
-  [ "lukas-reineke/cmp-under-comparator" ] = {},
+  ["lukas-reineke/cmp-rg"] = {},
+  ["lukas-reineke/cmp-under-comparator"] = {},
   -- [ "hrsh7th/cmp-nvim-lsp-document-symbol" ],
-  [ "ray-x/cmp-treesitter" ] = {},
-  [ "hrsh7th/cmp-emoji" ] = {},
-  [ "hrsh7th/cmp-calc" ] = {},
-  [ "iamcco/markdown-preview.nvim" ] = {
+  ["ray-x/cmp-treesitter"] = {},
+  ["hrsh7th/cmp-emoji"] = {},
+  ["hrsh7th/cmp-calc"] = {},
+  ["iamcco/markdown-preview.nvim"] = {
     run = ":call mkdp#util#install()",
     ft = "markdown",
   },
-  [ "mattn/gist-vim" ] = { -- native gist support
+  ["mattn/gist-vim"] = { -- native gist support
     requires = { "mattn/webapi-vim" },
     config = function()
       vim.g.gist_detect_filetype = true
     end,
   },
-  [ "stevearc/dressing.nvim" ] = {}, -- UI pretty
-  [ "numToStr/FTerm.nvim" ] = {}, -- floating term
-  [ "phaazon/hop.nvim" ] = {
+  ["stevearc/dressing.nvim"] = {}, -- UI pretty
+  ["numToStr/FTerm.nvim"] = {}, -- floating term
+  ["phaazon/hop.nvim"] = {
     branch = "v1", -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
       require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
     end,
   },
-  [ "tpope/vim-fugitive" ] = {}, -- git ops
-  [ "nvim-neorg/neorg" ] = {
+  ["tpope/vim-fugitive"] = {}, -- git ops
+  ["nvim-neorg/neorg"] = {
     config = function()
       require("neorg").setup({
         load = {
@@ -133,7 +133,7 @@ return {
     end,
     requires = "nvim-lua/plenary.nvim",
   },
-  [ "chip/telescope-software-licenses.nvim" ] = { -- easily insert FOSS licenses
+  ["chip/telescope-software-licenses.nvim"] = { -- easily insert FOSS licenses
     requires = {
       "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
@@ -143,7 +143,7 @@ return {
       require("telescope").load_extension("software-licenses")
     end,
   },
-  [ "nvim-telescope/telescope-symbols.nvim" ] = { -- easily insert FOSS licenses
+  ["nvim-telescope/telescope-symbols.nvim"] = { -- easily insert FOSS licenses
     requires = {
       "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
@@ -153,7 +153,7 @@ return {
       require("telescope.builtin").symbols({ sources = { "emoji", "kaomoji", "gitmoji" } })
     end,
   },
-  [ "nvim-telescope/telescope-dap.nvim" ] = { -- dap
+  ["nvim-telescope/telescope-dap.nvim"] = { -- dap
     requires = {
       "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
@@ -163,7 +163,7 @@ return {
       require("telescope").load_extension("dap")
     end,
   },
-  [ "mfussenegger/nvim-dap" ] = { -- debug adapters support
+  ["mfussenegger/nvim-dap"] = { -- debug adapters support
     config = function()
       local dap = require("dap")
       dap.adapters.ruby = {
@@ -198,31 +198,31 @@ return {
       }
     end,
   },
-  [ "chrisbra/csv.vim" ] = { -- way better CSV experience
+  ["chrisbra/csv.vim"] = { -- way better CSV experience
     ft = "csv",
     config = function()
       vim.g.csv_delim_test = ",;|"
       vim.g.csv_default_delim = ","
     end,
   },
-  [ "leoluz/nvim-dap-go" ] = {},
-  [ "mfussenegger/nvim-dap-python" ] = {
+  ["leoluz/nvim-dap-go"] = {},
+  ["mfussenegger/nvim-dap-python"] = {
     config = function()
       require("dap-python").setup("python3")
     end,
   },
-  [ "suketa/nvim-dap-ruby" ] = {
+  ["suketa/nvim-dap-ruby"] = {
     config = function()
       require("dap-ruby").setup()
     end,
   },
-  [ "rcarriga/nvim-dap-ui" ] = {
+  ["rcarriga/nvim-dap-ui"] = {
     requires = { "mfussenegger/nvim-dap" },
     config = function()
       require("dapui").setup()
     end,
   },
-  [ "powerman/vim-plugin-AnsiEsc" ] = {}, -- read ansi color escapes
+  ["powerman/vim-plugin-AnsiEsc"] = {}, -- read ansi color escapes
   ["mrjones2014/smart-splits.nvim"] = {
     config = function()
       require("smart-splits").ignored_buftypes = { "NvimTree" }
@@ -244,8 +244,8 @@ return {
       vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
     end,
   },
-  [ "kdheepak/lazygit.nvim" ] = {}, -- Git TUI
-  [ "AckslD/nvim-trevJ.lua" ] = {
+  ["kdheepak/lazygit.nvim"] = {}, -- Git TUI
+  ["AckslD/nvim-trevJ.lua"] = {
     config = 'require("trevj").setup()', -- optional call for configurating non-default filetypes etc
 
     -- uncomment if you want to lazy load
@@ -259,5 +259,7 @@ return {
     end,
   },
   ["dhruvasagar/vim-table-mode"] = {}, -- great Markdown table support
-  [ "fatih/vim-go" ] = {}, -- go support
+  ["fatih/vim-go"] = {}, -- go support
+  ["earthly/earthly.vim"] = {}, -- syntax highlighting for earthly cicd
+  ["nvim-treesitter/nvim-treesitter-context"] = {},
 }
