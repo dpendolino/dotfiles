1f5a09cad4ee958da3563b157f535fc505debab5
Daniel Pendolino <dpendolino@springhealth.com>
2022-06-27 15:53:17 -0400

    add vale and hover diagnostics

--- private_dot_config/nvchad/lua/custom/plugins/null-ls.lua
+++ private_dot_config/nvchad/lua/custom/plugins/null-ls.lua
@@ -26,6 +26,9 @@ local sources = {
 
   -- Yaml
   b.diagnostics.yamllint,
+
+  -- Vale
+  b.diagnostics.vale,
 }
 
 local M = {}
@@ -61,6 +64,20 @@ M.setup = function()
           end,
         })
       end
+      vim.api.nvim_create_autocmd("CursorHold", {
+        buffer = bufnr,
+        callback = function()
+          local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = "rounded",
+            source = "always",
+            prefix = " ",
+            scope = "cursor",
+          }
+          vim.diagnostic.open_float(nil, opts)
+        end
+      })
     end,
   })
 end
