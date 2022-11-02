94b026e7de335685dc620f88025279105e850798
Daniel Pendolino <dpendolino@gmail.com>
2022-11-02 10:53:52 -0400

    add neovim plugin to toggle booleans

--- private_dot_config/nvchad/lua/custom/plugins/init.lua
+++ private_dot_config/nvchad/lua/custom/plugins/init.lua
@@ -386,4 +386,19 @@ return {
     end,
     after = "telescope.nvim",
   },
+  ["nat-418/boole.nvim"] = {
+    config = function()
+      require("boole").setup({
+        mappings = {
+          increment = "<C-a>",
+          decrement = "<C-x>",
+        },
+        -- User defined loops
+        additions = {
+          { "Foo", "Bar" },
+          { "tic", "tac", "toe" },
+        },
+      })
+    end,
+  },
 }
