ad7817afa8d6736486b7f291f6570b80f2ac25e8
Daniel Pendolino <dpendolino@gmail.com>
2024-02-02 18:01:00 -0500

    switch from Packer to Lazy

--- .chezmoiscripts/run_nvchad.sh
+++ .chezmoiscripts/run_nvchad.sh
@@ -5,6 +5,4 @@ if [ -d ~/.config/nvim ] && [ -d ~/.config/nvchad ]; then
 	fi
 fi
 
-nvim --headless +'hi NormalFloat guibg=#1e222a' +PackerCompile +qa
-nvim --headless +'hi NormalFloat guibg=#1e222a' +PackerSync +qa
-nvim --headless +'hi NormalFloat guibg=#1e222a' +PackerInstall +qa
+nvim --headless "+Lazy! sync" +qa
