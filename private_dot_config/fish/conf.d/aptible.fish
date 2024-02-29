558c7930fabb5eff3537b1cb813960a58d3e4b05
Daniel Pendolino <dpendolino@gmail.com>
2024-02-29 08:48:28 -0500

    clean up fish configs

--- private_dot_config/fish/conf.d/aptible.fish
+++
@@ -1,5 +0,0 @@
-set -x APTIBLE_OUTPUT_FORMAT json
-
-function aptible-login
-  aptible login --email (op get item Aptible --fields email) --password (op get item Aptible --fields password) --otp-token (op get totp Aptible)
-end
